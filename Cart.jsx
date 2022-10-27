f408be0bed5515a625ba98c1dc346420674b8236
mrlado <33937546+mrlado@users.noreply.github.com>
2022-10-27 14:35:48 +0300

    Cart Container

---
+++ Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+const Cart = () => {
+  return (
+    <div className='CardContainer'>
+       <div className='wrapper'>
+        </div> 
+    </div>
+  )
+}
+
+export default Cart
